e21af35907f272ca37600b3254a463ab128b3245
sandra9ermak <sandra1ermak@gmail.com>
2021-11-09 19:53:42 +0200

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,7 @@ import Notification from "../components/Notification/Notification";
 import styles from "../components/Feedback/Feedback.module.css";
 import { useState } from "react";
 
-export const App = () => {
+const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
